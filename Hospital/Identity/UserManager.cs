f5a7a8b6bd8121605f61a88370224b845bfa2d2e
Brad McCormack <bradmccormack100@gmail.com>
2014-08-09 15:04:57 +1000

    - Hashing changes

---
+++ Hospital/Identity/UserManager.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hospital.Models;
+using PasswordHash;
+
+namespace Hospital.Identity
+{
+    public class BMUserManager : UserManager<User>
+    {
+        public BMUserManager() : base(new UserStore())
+        {
+            this.PasswordHasher = new SQLPasswordHasher();
+        }
+    }
+
+    public class SQLPasswordHasher : PasswordHasher
+    {
+        public override string HashPassword(string password)
+        {
+            return base.HashPassword(password);
+        }
+
+        public override PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword)
+        {
+            String providedPasswordHash = PasswordHash.PasswordHash.CreateHash(providedPassword);
+
+            if(PasswordHash.PasswordHash.ValidatePassword(hashedPassword, providedPasswordHash))
+                return PasswordVerificationResult.Success;
+            else
+                return PasswordVerificationResult.Failed;
+        }
+
+        private string EncryptPassword(string pass, int passwordFormat, string salt)
+        {
+            if (passwordFormat == 0) // MembershipPasswordFormat.Clear
+                return pass;
+
+            if (passwordFormat == 1)
+            {
+                String hash = PasswordHash.PasswordHash.CreateHash(pass);
+                return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(hash));
+            }
+            return String.Empty;
+        }
+
+
+    }
+}
