f598cc3b3c126fc9f1fd89f37cbcca6417ee8271
Brad McCormack <bradmccormack100@gmail.com>
2014-08-10 14:20:02 +1000

    - Add in server side validation for new patient

--- Hospital/Models/IdentityModels.cs
+++ Hospital/Models/IdentityModels.cs
@@ -27,205 +27,51 @@ namespace Hospital.Models
         private decimal _Rateperday;
         private string _BedType;
 
-
-        public int Id
-        {
-            get
-            {
-                return _Id;
-            }
-            set
-            {
-                _Id = value;
-            }
-        }
-
-        public string Name
-        {
-            get
-            {
-                return _Name;
-            }
-            set
-            {
-                _Name = value;
-            }
-        }
-
-        public decimal RatePerDay
-        {
-            get
-            {
-                return  _Rateperday;
-            }
-            set
-            {
-                _Rateperday = value;
-            }
-        }
-
-        public string BedType
-        {
-            get
-            {
-                return _BedType;
-            }
-            set
-            {
-                _BedType = value;
-            }
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal RatePerDay { get; set; }
+        public string BedType { get; set; }
     }
 
     public class Doctor : IDoctor
     {
-        private int _Id;
-        private String _Name;
-        private String _Address;
-        private String _Phone;
-
-        public int Id
-        {
-            get
-            {
-                return _Id;
-            }
-            set
-            {
-                _Id = value;
-            }
-        }
-
-        public string Name
-        {
-            get
-            {
-                return _Name;
-            }
-            set
-            {
-                _Name = value;
-            }
-        }
-
-        public string Address
-        {
-            get
-            {
-                return _Address;
-            }
-            set
-            {
-                _Address = value;
-            }
-        }
-
-        public string Phone
-        {
-            get
-            {
-                return _Phone;
-            }
-            set
-            {
-                _Phone = value;
-            }
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
     }
 
     public class Patient : IPatient
     {
-        private int _Id;
-        private String _Name;
-        private String _Address;
-        private DateTime _DOB;
-        private String _Phone;
-        private String _Emergency;
-        private DateTime _DOR;
+        public int Id { get; set; }
 
-        public int Id
-        {
-            get
-            {
-                return _Id;
-            }
-            set
-            {
-                _Id = value;
-            }
-        }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, ErrorMessage = "Must be under 50 characters")]
+        public String Name { get; set; }
 
-        public string Name
-        {
-            get
-            {
-                return _Name;
-            }
-            set
-            {
-                _Name = value;
-            }
-        }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(255, ErrorMessage = "Must be under 255 characters")]
+        public String Address { get; set; }
 
-        public string Address
-        {
-            get
-            {
-                return _Address;
-            }
-            set
-            {
-                _Address = value;
-            }
-        }
-
-        public DateTime DateOfBirth
-        {
-            get
-            {
-                return _DOB;
-            }
-            set
-            {
-                _DOB = value;
-            }
-        }
-
-        public string Phone
-        {
-            get
-            {
-                return _Phone;
-            }
-            set
-            {
-                _Phone = value;
-            }
-        }
-
-        public string EmergencyContact
-        {
-            get
-            {
-                return _Emergency;
-            }
-            set
-            {
-                _Emergency = value;
-            }
-        }
-
-        public DateTime DateOfRegistration
-        {
-            get
-            {
-                return _DOR;
-            }
-            set
-            {
-                _DOR = value;
-            }
-        }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(20, ErrorMessage = "Must be under 20 characters")]
+        public string Phone { get; set; }
+         
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(255, ErrorMessage = "Must be under 20 characters")]
+        public string EmergencyContact { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfRegistration { get; set; }
     }
 
     #region Visit
