43a3333f7a17837a3c958015b5795138b73878cd
Brad McCormack <bradmccormack100@gmail.com>
2014-08-10 16:22:15 +1000

    removed some junk

---
+++ Hospital/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Error404()
+        {
+            return View();
+        }
+    
+   
+    }
+}
